f5870f97a34a25444db5130884d75a249d888297
Hubert Obrzut <skoczek133@gmail.com>
2021-04-08 00:34:38 +0200

    added own static vector impl start

--- tests/static_rvector/main.cpp
+++ tests/static_rvector/main.cpp
@@ -1,11 +1,40 @@
-#include "static_vector.hpp"
+#include <boost/container/static_vector.hpp>
+#include "static_rvector.hpp"
+#include "static_vector/include/palotasb/static_vector.hpp"
+
+#include <chrono>
+#include <iostream>
+#include <vector>
 
 using T = int;
-constexpr int C = 100;
-using svector = static_rvector<T, C>;
+constexpr uint C = 50000;
+
+template<typename Vector, uint TIMES=C>
+void test(const std::string& name) {
+	auto start = std::chrono::high_resolution_clock::now(); 
+
+	uint sum = 0;
+	for (uint i = 0; i < TIMES; ++i) {
+		Vector v;
+		for (uint i = 0; i < TIMES; ++i)
+			v.emplace_back(i);
+		for (auto x : v)
+			sum += x;
+	}
+	std::cout << sum << std::endl;
+
+	auto end = std::chrono::high_resolution_clock::now();
+	auto delta = end - start;
+	auto elapsed = std::chrono::duration<double>(end - start).count();
+	std::cout << name << ": " << elapsed * 1000 << "ms" << std::endl;
+}
 
 int main() {
-	svector v;
+	test<boost::container::static_vector<T, C>>("boost::static_vector");
+	// test<stlpb::static_vector<T, C>>("stlpb::static_vector");
+	test<static_rvector<T, C>>("static_rvector");
+	// test<std::vector<T>>("std::vector");
+	// test<boost::container::vector<T>>("boost::vector");
 
 	return 0;
 }
--- tests/static_rvector/static_rvector.hpp
+++ tests/static_rvector/static_rvector.hpp
@@ -1,9 +1,107 @@
 #pragma once
 
-#include <cstdint>
+#include <iostream>
 
-template<typename T, uint C>
+using cap_type = size_t;
+
+template<typename T, cap_type C>
 class static_rvector {
-private:
+public:
+	using value_type = T;
+	using size_type = cap_type;
+	using reference = T&;
+	using const_reference = const T&;
+	using iterator = T*;
+	using const_iterator = const T*;
 
+	static constexpr cap_type Capacity = C;
+
+	static_rvector();
+
+	iterator begin() noexcept;
+	const_iterator begin() const noexcept;
+	iterator end() noexcept;
+	const_iterator end() const noexcept;
+
+	template<typename... Args>
+	void emplace_back(Args&&... args);
+	template<typename... Args>
+	void fast_emplace_back(Args&&... args);
+
+	void push_back(const_reference value) noexcept;
+	void fast_push_back(const_reference value) noexcept;
+	void push_back(value_type&& value);
+	void fast_push_back(value_type&& value) noexcept;
+
+private:
+	T data_[C];
+	size_type length_;
 };
+
+template<typename T, cap_type C>
+static_rvector<T, C>::static_rvector() :
+	length_(0) {
+}
+
+template<typename T, cap_type C>
+typename static_rvector<T, C>::iterator
+static_rvector<T, C>::begin() noexcept {
+	return data_;
+}
+
+template<typename T, cap_type C>
+typename static_rvector<T, C>::const_iterator
+static_rvector<T, C>::begin() const noexcept {
+	return data_;
+}
+
+template<typename T, cap_type C>
+typename static_rvector<T, C>::iterator
+static_rvector<T, C>::end() noexcept {
+	return data_ + length_;
+}
+
+template<typename T, cap_type C>
+typename static_rvector<T, C>::const_iterator
+static_rvector<T, C>::end() const noexcept {
+	return data_ + length_;
+}
+
+template<typename T, cap_type C>
+template<typename... Args>
+void static_rvector<T, C>::emplace_back(Args&&... args) {
+	if (length_ == Capacity)
+		throw std::out_of_range("msg");
+	data_[length_++] = value_type(std::forward<Args>(args)...);
+}
+
+template<typename T, cap_type C>
+template<typename... Args>
+void static_rvector<T, C>::fast_emplace_back(Args&&... args) {
+	data_[length_++] = value_type(std::forward<Args>(args)...);
+}
+
+template<typename T, cap_type C>
+void static_rvector<T, C>::push_back(const_reference value) noexcept {
+	if (length_ == Capacity)
+		throw std::out_of_range("msg");
+	data_[length_++] = value;
+}
+
+template<typename T, cap_type C>
+void static_rvector<T, C>::fast_push_back(const_reference value) noexcept {
+	data_[length_++] = value;
+}
+
+template<typename T, cap_type C>
+void static_rvector<T, C>::push_back(value_type&& value) {
+	if (length_ == Capacity)
+		throw std::out_of_range("msg");
+	data_[length_++] = std::forward<value_type>(value);
+}
+
+template<typename T, cap_type C>
+void static_rvector<T, C>::fast_push_back(value_type&& value) noexcept {
+	data_[length_++] = std::forward<value_type>(value);
+	// new (&data_[length_++]) value_type(value);
+}
