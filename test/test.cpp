a72e69158ff9de04e73d1b74f8ec8a9422e33993
Hubert Obrzut <skoczek133@gmail.com>
2021-04-24 01:02:06 +0200

    testing playground file

---
+++ test/test.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <static_vector.hpp>
+#include <boost/container/static_vector.hpp>
+
+int main() {
+	using T = int;
+	constexpr int C = 5;
+	using Vector = uwr::static_vector<T, C>;
+	// using Vector = boost::container::static_vector<T, C>;
+	
+	Vector v(1);
+	v.erase(v.begin());
+
+	for (const auto& x : v)
+		std::cout << x << ' ';
+	std::cout << '\n';
+
+	return 0;
+}
