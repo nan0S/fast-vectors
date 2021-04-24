c887955e3f48830218aa14c7d19fd6c2962a748c
Hubert Obrzut <skoczek133@gmail.com>
2021-04-24 13:46:12 +0200

    repaired benchmark

--- test/benchmark.cpp
+++ test/benchmark.cpp
@@ -3,6 +3,7 @@
 #include <static_vector.hpp>
 #include <boost/container/static_vector.hpp>
 #include <boost/format.hpp>
+#include <test_type/test_type.hpp>
 
 template<template<class> class V, class... Ts>
 class vector_env {
@@ -12,12 +13,7 @@ public:
 
 	void run_simulation(int iters=1000) {
 		for (int i = 0; i < iters; ++i) {
-			try {
-				(dispatch_action<Ts>(i), ...);
-			}
-			catch (std::bad_alloc& e) {
-				std::cout << e.what() << '\n';
-			}
+			(dispatch_action<Ts>(i), ...);
 			// std::cout << boost::format("iter = %s, length_sum = %ld\n") % i % get_length_sum();
 		}
 	}
@@ -31,8 +27,10 @@ private:
 	size_t get_typed_length_sum() {
 		auto& typed_env = get_env_of_type<T>();
 		size_t length_sum = 0;
+
 		for (const auto& v : typed_env)
 			length_sum += v.size();
+
 		return length_sum;
 	}
 
@@ -40,9 +38,10 @@ private:
 	void dispatch_action(int i) {
 		using namespace std::placeholders;
 		std::function<void(int)> fs[] = {
+			std::bind(&vector_env<V, Ts...>::construct_action<T>, this, _1),
+			std::bind(&vector_env<V, Ts...>::destroy_action<T>, this, _1),
 			std::bind(&vector_env<V, Ts...>::push_back_action<T>, this, _1),
 			std::bind(&vector_env<V, Ts...>::pop_back_action<T>, this, _1),
-			std::bind(&vector_env<V, Ts...>::construct_action<T>, this, _1),
 			std::bind(&vector_env<V, Ts...>::copy_action<T>, this, _1),
 			std::bind(&vector_env<V, Ts...>::insert_action<T>, this, _1),
 			std::bind(&vector_env<V, Ts...>::erase_action<T>, this, _1),
@@ -54,8 +53,41 @@ private:
 	}
 
 	template<class T>
+	void construct_action(int i) {
+		// std::cout << boost::format("construct_action(%d)\n") % i;
+
+		auto& typed_env = get_env_of_type<T>();
+		int q = random(1, 3);
+		std::uniform_int_distribution<> size_dist(1, i + 10);
+
+		while (q--)
+			typed_env.emplace_back(size_dist(gen));
+	}
+
+	template<class T>
+	void destroy_action(int i) {
+		// std::cout << boost::format("destroy_action(%d)\n") % i;
+
+		auto& typed_env = get_env_of_type<T>();
+		int typed_env_size = typed_env.size();
+		if (typed_env_size == 0) {
+			construct_action<T>(i);
+			return;
+		}
+		
+		int q = random(1, std::min(3, typed_env_size));
+
+		while (q--) {
+			int pick = random(0, --typed_env_size);
+
+			std::swap(typed_env[pick], typed_env.back());
+			typed_env.pop_back();
+		}
+	}
+
+	template<class T>
 	void push_back_action(int i) {
-		std::cout << boost::format("push_back_action(%d)\n") % i;
+		// std::cout << boost::format("push_back_action(%d)\n") % i;
 
 		auto& typed_env = get_env_of_type<T>();
 		int typed_env_size = typed_env.size();
@@ -64,19 +96,15 @@ private:
 			return;
 		}
 
-		std::cout << "t1\n";
-
 		// TODO: maybe sth more complex (?)
 		int q = random(1, typed_env_size / 3 + 1);
 		std::uniform_int_distribution<> pick_dist(0, typed_env_size - 1);
-		std::uniform_int_distribution<> size_dist(1, i * 100 + 1);
-		std::cout << "t2\n";
-		std::cout << q << std::endl;
+		size_type<T> max_size = i * 100 + 1;
 
 		while (q--) {
 			auto& picked = typed_env[pick_dist(gen)];
-			auto put_in = size_dist(gen);
-			std::cout << "t3\n";
+			auto can_put_in = picked.capacity() - picked.size();
+			auto put_in = random<size_type<T>>(0, std::min(max_size, can_put_in));
 
 			while (put_in--)
 				picked.emplace_back();
@@ -85,7 +113,7 @@ private:
 
 	template<class T>
 	void pop_back_action(int i) {
-		std::cout << boost::format("pop_back_action(%d)\n") % i;
+		// std::cout << boost::format("pop_back_action(%d)\n") % i;
 
 		auto& typed_env = get_env_of_type<T>();
 		int typed_env_size = typed_env.size();
@@ -107,20 +135,8 @@ private:
 	}
 
 	template<class T>
-	void construct_action(int i) {
-		std::cout << boost::format("construct_action(%d)\n") % i;
-
-		auto& typed_env = get_env_of_type<T>();
-		int q = random(1, 3);
-		std::uniform_int_distribution<> size_dist(1, i + 10);
-
-		while (q--)
-			typed_env.emplace_back(size_dist(gen));
-	}
-
-	template<class T>
 	void copy_action(int i) {
-		std::cout << boost::format("copy_action(%d)\n") % i;
+		// std::cout << boost::format("copy_action(%d)\n") % i;
 
 		auto& typed_env = get_env_of_type<T>();
 		int typed_env_size = typed_env.size();
@@ -142,7 +158,7 @@ private:
 
 	template<class T>
 	void insert_action(int i) {
-		std::cout << boost::format("insert_action(%d)\n") % i;
+		// std::cout << boost::format("insert_action(%d)\n") % i;
 
 		auto& typed_env = get_env_of_type<T>();
 		int typed_env_size = typed_env.size();
@@ -153,11 +169,12 @@ private:
 
 		int q = random(1, typed_env_size / 3 + 1);
 		std::uniform_int_distribution<> pick_dist(0, typed_env_size - 1);
-		std::uniform_int_distribution<> size_dist(1, i + 1);
+		size_type<T> max_insert = i + 1;
 
 		while (q--) {
 			auto& picked = typed_env[pick_dist(gen)];
-			auto cnt = size_dist(gen);
+			auto can_insert = picked.capacity() - picked.size();
+			auto cnt = random<size_type<T>>(0, std::min(can_insert, max_insert));
 			auto pos = random<size_type<T>>(0, picked.size());
 
 			picked.insert(picked.begin() + pos, cnt, T());
@@ -166,7 +183,7 @@ private:
 
 	template<class T>
 	void erase_action(int i) {
-		std::cout << boost::format("erase_action(%d)\n") % i;
+		// std::cout << boost::format("erase_action(%d)\n") % i;
 
 		auto& typed_env = get_env_of_type<T>();
 		int typed_env_size = typed_env.size();
@@ -241,10 +258,12 @@ void push_back_benchmark(std::string test_name, int max_iters=100000, int repeat
 template<template<class> class V, class... Ts>
 void experiment(std::string test_name, int iters=1000, int repeat=10) {
 	auto begin = std::chrono::high_resolution_clock::now();
+	constexpr int seed = 12345512;
 
-	for(int seed = 12345512; seed < 12345512 + repeat; seed++) {
-		vector_env<V, Ts...> v_env(seed);
+	for (int r = 0; r < repeat; ++r) {
+		vector_env<V, Ts...> v_env(seed + r);
 		v_env.run_simulation(iters);
+		std::cout << boost::format("repeat: %d/%d\n") % (r + 1) % repeat;
 	}
 
 	auto end = std::chrono::high_resolution_clock::now();
@@ -253,7 +272,7 @@ void experiment(std::string test_name, int iters=1000, int repeat=10) {
 	std::cout << test_name << ": " << delta * 1000 << "ms" << std::endl;
 }
 
-static constexpr uint C = 50000;
+static constexpr uint C = 500000;
 
 template<class T>
 using ustatic_vector = uwr::static_vector<T, C>;
@@ -267,10 +286,21 @@ int main() {
 	// push_back_benchmark<ustatic_vector<int>>(
 		// "uwr::static_vector", C, 1000);
 	
-	experiment<bstatic_vector, int, std::string>(
-		"boost::static_vector<int, std::string>", 1000, 10);
-	// experiment<ustatic_vector, int, std::string>(
-		// "uwr::static_vector<int, std::string>", 1000, 10);
+	experiment<bstatic_vector, int>(
+		"boost::static_vector<int>", 1000, 10);
+	experiment<ustatic_vector, int>(
+		"uwr::static_vector<int>", 1000, 10);
+
+	experiment<bstatic_vector, std::string>(
+		"boost::static_vector<std::string>", 1000, 10);
+	experiment<ustatic_vector, std::string>(
+		"uwr::static_vector<std::string>", 1000, 10);
+
+	test_type::do_print = false;
+	experiment<bstatic_vector, test_type>(
+		"boost::static_vector<test_type>", 1000, 10);
+	experiment<ustatic_vector, test_type>(
+		"uwr::static_vector<test_type>", 1000, 10);
 
 	return 0;
 }
