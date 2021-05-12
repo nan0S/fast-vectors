5ce3b4646f1de27b390df52a0d697490c0b6ac38
Hubert Obrzut <skoczek133@gmail.com>
2021-05-12 23:26:53 +0200

    changed typename to class

--- include/common/memory.hpp
+++ include/common/memory.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "../common/trivial.hpp"
+#include "trivial.hpp"
 #include <algorithm>
 #include <cstring>
 
@@ -11,170 +11,184 @@ typedef unsigned int uint;
 // using len_t = uint;
 using len_t = std::size_t;
 
-template<typename T>
+template<class T>
 void construct(T* begin, T* end);
-template<typename T>
+template<class T>
 void construct(T* data, len_t n);
 
 /* regular fill */
-template<typename T>
+template<class T>
 void fill(T* begin, T* end, const T& val);
-template<typename T>
+template<class T>
 void fill(T* data, len_t n, const T& val);
 
 /* uninitialized fill */
-template<typename T>
+template<class T>
 void ufill(T* begin, T* end, const T& val);
-template<typename T>
+template<class T>
 void ufill(T* data, len_t n, const T& val);
 
 /* regular copy */
-template<typename T, typename InputIterator>
+template<class T, class InputIterator>
 T_Copy_A<T> copy(T* data, InputIterator begin, len_t n);
-template<typename T, typename InputIterator>
+template<class T, class InputIterator>
 NT_Copy_A<T> copy(T* data, InputIterator begin, len_t n);
 
 /* uninitialized copy */
-template<typename T, typename InputIterator>
+template<class T, class InputIterator>
 T_Copy_C<T> ucopy(T* data, InputIterator begin, InputIterator end);
-template<typename T, typename InputIterator>
+template<class T, class InputIterator>
 NT_Copy_C<T> ucopy(T* data, InputIterator begin, InputIterator end);
 
 /* regular move */
-template<typename T, typename InputIterator>
+template<class T, class InputIterator>
 T_Move_A<T> move(T* data, InputIterator begin, InputIterator end);
-template<typename T, typename InputIterator>
+template<class T, class InputIterator>
 NT_Move_A<T> move(T* data, InputIterator begin, InputIterator end);
 
 /* uninitialized move */
-template<typename T, typename InputIterator>
+template<class T, class InputIterator>
+T_Move_C<T> umove(T* data, InputIterator begin, len_t n);
+template<class T, class InputIterator>
+NT_Move_C<T> umove(T* data, InputIterator begin, len_t n);
+template<class T, class InputIterator>
 T_Move_C<T> umove(T* data, InputIterator begin, InputIterator end);
-template<typename T, typename InputIterator>
+template<class T, class InputIterator>
 NT_Move_C<T> umove(T* data, InputIterator begin, InputIterator end);
 
-template<typename T>
-void destroy(T* begin, T* end);
-template<typename T>
+template<class T>
 void destroy(T* data, len_t n);
+template<class T>
+void destroy(T* begin, T* end);
 
-template<typename T>
+template<class T>
 void destroy_at(T* addr);
 
 /* assumption: begin < dest <= end and addresses >= end are uninitialized*/
-template<typename T>
+template<class T>
 T_Move<T> shiftr(T* dest, T* begin, T* end);
-template<typename T>
+template<class T>
 NT_Move<T> shiftr(T* dest, T* begin, T* end);
 
 /* assumption: dest < begin */
-template<typename T>
+template<class T>
 T_Move_A<T> shiftl(T* dest, T* begin, T* end);
-template<typename T>
+template<class T>
 NT_Move_A<T> shiftl(T* dest, T* begin, T* end);
 
-template<typename T>
+template<class T>
 void construct(T* begin, T* end) {
 	std::uninitialized_default_construct(begin, end);
 }
 
-template<typename T>
+template<class T>
 void construct(T* data, len_t n) {
 	std::uninitialized_default_construct_n(data, n);
 }
 
-template<typename T>
+template<class T>
 void fill(T* begin, T* end, const T& val) {
 	std::fill(begin, end, val);
 }
 
-template<typename T>
+template<class T>
 void fill(T* data, len_t n, const T& val) {
 	std::fill_n(data, n, val);
 }
 
-template<typename T>
+template<class T>
 void ufill(T* begin, T* end, const T& val) {
 	std::uninitialized_fill(begin, end, val);
 }
 
-template<typename T>
+template<class T>
 void ufill(T* data, len_t n, const T& val) {
 	std::uninitialized_fill_n(data, n, val);
 }
 
-template<typename T, typename InputIterator>
+template<class T, class InputIterator>
 T_Copy_A<T> copy(T* data, InputIterator begin, len_t n) {
 	std::memcpy(data, &*begin, n * sizeof(T));
 }
 
-template<typename T, typename InputIterator>
+template<class T, class InputIterator>
 NT_Copy_A<T> copy(T* data, InputIterator begin, len_t n) {
 	std::copy_n(begin, n, data);
 }
 
-template<typename T, typename InputIterator>
+template<class T, class InputIterator>
 T_Copy_C<T> ucopy(T* data, InputIterator begin, InputIterator end) {
 	std::memcpy(data, &*begin, (end - begin) * sizeof(T));
 }
 
-template<typename T, typename InputIterator>
+template<class T, class InputIterator>
 NT_Copy_C<T> ucopy(T* data, InputIterator begin, InputIterator end) {
 	std::uninitialized_copy(begin, end, data);
 }
 
-template<typename T, typename InputIterator>
+template<class T, class InputIterator>
 T_Move_A<T> move(T* data, InputIterator begin, len_t n) {
 	std::memcpy(data, &*begin, n * sizeof(T));
 }
 
-template<typename T, typename InputIterator>
+template<class T, class InputIterator>
 NT_Move_A<T> move(T* data, InputIterator begin, len_t n) {
 	std::move(begin, begin + n, data);
 }
 
-template<typename T, typename InputIterator>
+template<class T, class InputIterator>
+T_Move_C<T> umove(T* data, InputIterator begin, len_t n) {
+	std::memcpy(data, &*begin, n * sizeof(T));
+}
+
+template<class T, class InputIterator>
+NT_Move_C<T> umove(T* data, InputIterator begin, len_t n) {
+    std::uninitialized_move_n(begin, n, data);
+}
+
+template<class T, class InputIterator>
 T_Move_C<T> umove(T* data, InputIterator begin, InputIterator end) {
 	std::memcpy(data, &*begin, (end - begin) * sizeof(T));
 }
 
-template<typename T, typename InputIterator>
+template<class T, class InputIterator>
 NT_Move_C<T> umove(T* data, InputIterator begin, InputIterator end) {
 	std::uninitialized_move(begin, end, data);
 }
 
-template<typename T>
+template<class T>
+void destroy(T* data, len_t n) {
+    std::destroy_n(data, n);
+}
+
+template<class T>
 void destroy(T* begin, T* end) {
 	std::destroy(begin, end);
 }
 
-template<typename T>
-void destroy(T* data, len_t n) {
-	std::destroy_n(data, n);
-}
-
-template<typename T>
+template<class T>
 void destroy_at(T* addr) {
 	std::destroy_at(addr);
 }
 
-template<typename T>
+template<class T>
 T_Move<T> shiftr(T* dest, T* begin, T* end) {
 	std::memmove(dest, begin, (end - begin) * sizeof(T));
 }
 
-template<typename T>
+template<class T>
 NT_Move<T> shiftr(T* dest, T* begin, T* end) {
 	T* seg = end - (dest - begin);
 	umove(end, seg, end);
 	std::move_backward(begin, seg, end);
 }
 
-template<typename T>
+template<class T>
 T_Move_A<T> shiftl(T* dest, T* begin, T* end) {
 	std::memmove(dest, begin, (end - begin) * sizeof(T));
 }
 
-template<typename T>
+template<class T>
 NT_Move_A<T> shiftl(T* dest, T* begin, T* end) {
 	std::move(begin, end, dest);
 }
--- include/common/trivial.hpp
+++ include/common/trivial.hpp
@@ -4,107 +4,107 @@
 
 namespace uwr {
 
-template<typename T>
+template<class T>
 struct is_trivially_copyable {
 	static constexpr bool value =
 		std::is_trivially_copyable_v<T>;
 };
 
-template<typename T>
+template<class T>
 struct is_trivially_moveable {
 	static constexpr bool value =
 		std::is_trivially_move_constructible_v<T> &&
 		std::is_trivially_move_assignable_v<T>;
 };
 
-template<typename T>
+template<class T>
 struct is_trivially_copy_constructible {
 	static constexpr bool value =
 		std::is_trivially_copy_constructible_v<T>;
 };
 
-template<typename T>
+template<class T>
 struct is_trivially_move_constructible {
 	static constexpr bool value =
 		std::is_trivially_move_constructible_v<T>;
 };
 
-template<typename T>
+template<class T>
 struct is_trivially_copy_assignable {
 	static constexpr bool value =
 		std::is_trivially_copy_assignable_v<T>;
 };
 
-template<typename T>
+template<class T>
 struct is_trivially_move_assignable {
 	static constexpr bool value =
 		std::is_trivially_move_assignable_v<T>;
 };
 
-template<typename T>
+template<class T>
 inline constexpr bool is_trivially_copyable_v =
 	is_trivially_copyable<T>::value;
 
-template<typename T>
+template<class T>
 inline constexpr bool is_trivially_moveable_v =
 	is_trivially_moveable<T>::value;
 
-template<typename T>
+template<class T>
 inline constexpr bool is_trivially_copy_constructible_v =
 	is_trivially_copy_constructible<T>::value;
 
-template<typename T>
+template<class T>
 inline constexpr bool is_trivially_move_constructible_v =
 	is_trivially_move_constructible<T>::value;
 
-template<typename T>
+template<class T>
 inline constexpr bool is_trivially_copy_assignable_v =
 	is_trivially_copy_assignable<T>::value;
 
-template<typename T>
+template<class T>
 inline constexpr bool is_trivially_move_assignable_v =
 	is_trivially_move_assignable<T>::value;
 
-template<typename T>
+template<class T, class R = void>
 using T_Move =
-	std::enable_if_t<is_trivially_moveable_v<T>>;
-template<typename T>
+	std::enable_if_t<is_trivially_moveable_v<T>, R>;
+template<class T, class R = void>
 using NT_Move =
-	std::enable_if_t<!is_trivially_moveable_v<T>>;
+	std::enable_if_t<!is_trivially_moveable_v<T>, R>;
 
-template<typename T>
+template<class T, class R = void>
 using T_Copy_C =
-	std::enable_if_t<is_trivially_copy_constructible_v<T>>;
-template<typename T>
+	std::enable_if_t<is_trivially_copy_constructible_v<T>, R>;
+template<class T, class R = void>
 using NT_Copy_C =
-	std::enable_if_t<!is_trivially_copy_constructible_v<T>>;
+	std::enable_if_t<!is_trivially_copy_constructible_v<T>, R>;
 
-template<typename T>
+template<class T, class R = void>
 using T_Move_C =
-	std::enable_if_t<is_trivially_move_constructible_v<T>>;
-template<typename T>
+	std::enable_if_t<is_trivially_move_constructible_v<T>, R>;
+template<class T, class R = void>
 using NT_Move_C =
-	std::enable_if_t<!is_trivially_move_constructible_v<T>>;
+	std::enable_if_t<!is_trivially_move_constructible_v<T>, R>;
 
-template<typename T>
+template<class T, class R = void>
 using T_Copy_A =
-	std::enable_if_t<is_trivially_copy_assignable_v<T>>;
-template<typename T>
+	std::enable_if_t<is_trivially_copy_assignable_v<T>, R>;
+template<class T, class R = void>
 using NT_Copy_A =
-	std::enable_if_t<!is_trivially_copy_assignable_v<T>>;
+	std::enable_if_t<!is_trivially_copy_assignable_v<T>, R>;
 
-template<typename T>
+template<class T, class R = void>
 using T_Move_A =
-	std::enable_if_t<is_trivially_move_assignable_v<T>>;
-template<typename T>
+	std::enable_if_t<is_trivially_move_assignable_v<T>, R>;
+template<class T, class R = void>
 using NT_Move_A =
-	std::enable_if_t<!is_trivially_move_assignable_v<T>>;
+	std::enable_if_t<!is_trivially_move_assignable_v<T>, R>;
 
-template<typename T>
+template<class T, class R = void>
 using T_Copy =
-	std::enable_if_t<is_trivially_copyable_v<T>>;
-template<typename T>
+	std::enable_if_t<is_trivially_copyable_v<T>, R>;
+template<class T, class R = void>
 using NT_Copy =
-	std::enable_if_t<!is_trivially_copyable_v<T>>;
+	std::enable_if_t<!is_trivially_copyable_v<T>, R>;
 
 } // namespace uwr
