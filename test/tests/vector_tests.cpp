babcb7d562de7a3ec48bff8e7d62553d990ed567
Hubert Obrzut <skoczek133@gmail.com>
2021-05-17 23:27:30 +0200

    vector implementation - midwork

---
+++ test/tests/vector_tests.cpp
@@ -0,0 +1,17 @@
+#include <vector.hpp>
+
+template<class T>
+using vector_t = uwr::vector<T>;
+
+#include "vector_tests_base.hpp"
+
+template<class T>
+class VectorTests : public VectorTestsBase<T> {
+};
+
+TYPED_TEST_SUITE(VectorTests, TestedTypes, TypeNames);
+
+int main(int argc, char* argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
