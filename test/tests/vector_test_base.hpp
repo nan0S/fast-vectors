a8a1c2bca66dd7c2062230aa4e5fc79a1f33bddb
Hubert Obrzut <skoczek133@gmail.com>
2021-07-15 20:40:14 +0200

    std::out_of_range exception is no longer expected to be thrown in tests

--- test/tests/vector_test_base.hpp
+++ test/tests/vector_test_base.hpp
@@ -271,7 +271,7 @@ TYPED_TEST(VectorTestBaseFixture, AccessOperators) {
         EXPECT_EQ(v.at(i), this->GetValue(i));
     }
 
-    EXPECT_THROW(v.at(v.size()), std::out_of_range);
+    // EXPECT_THROW(v.at(v.size()), std::out_of_range);
 }
 
 TYPED_TEST(VectorTestBaseFixture, FrontBack) {
