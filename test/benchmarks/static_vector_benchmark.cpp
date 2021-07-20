6d1abc3ab53a0cb7d73706619133468db94127a2
Hubert Obrzut <skoczek133@gmail.com>
2021-07-20 21:43:16 +0200

    added COMMON_ITERS to static_vector_benchmark

--- test/benchmarks/static_vector_benchmark.cpp
+++ test/benchmarks/static_vector_benchmark.cpp
@@ -14,16 +14,18 @@ using uwr_static_vector = uwr::static_vector<T, C>;
 template<class T>
 using uwr_static_vector_alt = uwr::static_vector_alt<T, C>;
 
+#define COMMON_ITERS 100
+
 static constexpr int INT_TIMES = 500;
-#define INT_ITERS -1
+#define INT_ITERS (COMMON_ITERS == -1 ? -1 : COMMON_ITERS)
 static constexpr int STRING_TIMES = 300;
-#define STRING_ITERS -1
+#define STRING_ITERS (COMMON_ITERS == -1 ? -1 : COMMON_ITERS)
 static constexpr int TEST_TYPE_TIMES = 300;
-#define TEST_TYPE_ITERS -1
+#define TEST_TYPE_ITERS (COMMON_ITERS == -1 ? -1 : COMMON_ITERS)
 static constexpr int INT_STRING_TIMES = 200;
-#define INT_STRING_ITERS -1
+#define INT_STRING_ITERS (COMMON_ITERS == -1 ? -1 : COMMON_ITERS)
 static constexpr int INT_STRING_ARRAY_TIMES = 100;
-#define INT_STRING_ARRAY_ITERS -1
+#define INT_STRING_ARRAY_ITERS (COMMON_ITERS == -1 ? -1 : COMMON_ITERS)
 
 #define DO_STATIC_VECTOR_BENCH
 #define DO_STATIC_VECTOR_ALT_BENCH
