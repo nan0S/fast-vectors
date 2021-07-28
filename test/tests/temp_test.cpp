3ac101b6a18db613738a7c6d68e18c222983e69a
Hubert Obrzut <skoczek133@gmail.com>
2021-07-28 22:31:20 +0200

    current temporary test change

--- test/tests/temp_test.cpp
+++ test/tests/temp_test.cpp
@@ -4,39 +4,49 @@
 #include <test_type/test_type.hpp>
 #include <utils/utils.hpp>
 
+using namespace std;
+
+constexpr static int C = 10;
+
+template<class T>
+using uvector = uwr::static_vector<T, C>;
+template<class T>
+using bvector = boost::container::static_vector<T, C>;
+
+template<class Container>
+void print(const Container& c) {
+    for (const auto& x : c)
+        cout << x << " ";
+    cout << endl;
+}
+
 int main() {
-    constexpr static int C = 10;
     using T = test_type;
     using Vector1 = boost::container::static_vector<T, C>;
     using Vector2 = uwr::static_vector<T, C>;
 
-    Vector1 v1 { 1, 2, 3, 4, 5 };
-    Vector2 v2 { 1, 2, 3, 4, 5 };
-    const test_type val = 13;
-    int pos = 0;
+    // Vector1 v1 { 1, 2, 3, 4, 5 };
+    // Vector2 v2 { 1, 2, 3, 4, 5 };
+    // const test_type val = 13;
+    // int pos = 0;
 
     ON;
+    
+    uvector<int> v1(5);
+    print(v1);
 
-    v1.emplace(v1.begin() + pos, val);
-    std::cout << std::endl;
+    bvector<int> v2(5);
+    print(v2);
 
-    v2.emplace(v2.begin() + pos, val);
-    std::cout << std::endl;
+    vector<int> v3(5);
+    print(v3);
 
-    v1.emplace(v1.begin() + pos, 13);
-    std::cout << std::endl;
-
-    v2.emplace(v2.begin() + pos, 13);
-    std::cout << std::endl;
-
-    for (const auto& x : v1)
-        std::cout << x << " ";
-    std::cout << std::endl;
-
-    for (const auto& x : v2)
-        std::cout << x << " ";
-    std::cout << std::endl;
+    // auto s = sizeof(test_type);
+    // int n = 5;
+    // char t[n * s];
+    // std::uninitialized_default_construct_n((test_type*)t, n);
 
     OFF;
+
     return 0;
 }
