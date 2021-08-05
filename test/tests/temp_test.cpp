0362dfc902aebebbe737a1d88cafd69f64b5ffc6
Hubert Obrzut <skoczek133@gmail.com>
2021-08-05 22:50:36 +0200

    modify temp_test

--- test/tests/temp_test.cpp
+++ test/tests/temp_test.cpp
@@ -9,10 +9,10 @@ using namespace std;
 
 constexpr static int C = 10;
 
-template<class T>
-using uvector = uwr::static_vector<T, C>;
-template<class T>
-using bvector = boost::container::static_vector<T, C>;
+// template<class T>
+// using uvector = uwr::static_vector<T, C>;
+// template<class T>
+// using bvector = boost::container::static_vector<T, C>;
 
 template<class Container>
 void print(const Container& c) {
@@ -23,11 +23,17 @@ void print(const Container& c) {
 
 int main() {
     using T = test_type;
-    uwr::vector<T> v { 1 };
+    uwr::vector<T> v { 1, 2, 3, 4, 5 };
+    cout << v.capacity() << endl;
+    cout << sizeof(T) * v.capacity() << endl;
 
     ON;
-    
+
     OFF;
+    
+    for (const auto& x : v)
+        cout << x << " ";
+    cout << endl;
 
     return 0;
 }
