1dc45b19d1b077e44e723c625a7133046f2a0bc4
Hubert Obrzut <skoczek133@gmail.com>
2021-08-06 21:41:15 +0200

    add some investigating to temp_test about member functions

--- test/tests/temp_test.cpp
+++ test/tests/temp_test.cpp
@@ -4,15 +4,16 @@
 #include <vector.hpp>
 #include <test_type/test_type.hpp>
 #include <utils/utils.hpp>
+#include <rvector.h>
 
 using namespace std;
 
 constexpr static int C = 10;
 
-// template<class T>
-// using uvector = uwr::static_vector<T, C>;
-// template<class T>
-// using bvector = boost::container::static_vector<T, C>;
+using boost_gf = boost::container::growth_factor_100;
+using boost_options = boost::container::vector_options_t<boost::container::growth_factor<boost_gf>>;
+template<class T>
+using boost_vector = boost::container::vector<T, boost::container::new_allocator<T>, boost_options>; 
 
 template<class Container>
 void print(const Container& c) {
@@ -21,19 +22,34 @@ void print(const Container& c) {
     cout << endl;
 }
 
+template<class Vector>
+void f(const std::string& name) {
+    cout << endl << name << ":" << endl;
+    test_type::start_recording();
+    
+    Vector v(511);
+    cout << v.capacity() << endl;
+    test_type::start_recording();
+    v.emplace_back();
+    cout << v.capacity() << endl;
+    // v.insert(v.begin(), 10000, 1);
+
+    test_type::print_stats();
+    cout << endl;
+}
+
+#define RUN(v) f<v>(#v)
+
 int main() {
     using T = test_type;
-    uwr::vector<T> v { 1, 2, 3, 4, 5 };
-    cout << v.capacity() << endl;
-    cout << sizeof(T) * v.capacity() << endl;
+    // using Vector = uwr::vector<T>;
+    // using Vector = std::vector<T>;
+    // using Vector = boost_vector<T>;
 
-    ON;
-
-    OFF;
-    
-    for (const auto& x : v)
-        cout << x << " ";
-    cout << endl;
+    RUN(uwr::vector<T>);
+    RUN(std::vector<T>);
+    RUN(rvector<T>);
+    RUN(boost_vector<T>);
 
     return 0;
 }
