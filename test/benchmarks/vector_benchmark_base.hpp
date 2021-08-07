f2979a34f5b3f43b21e76058da66aac30c0f6418
Hubert Obrzut <skoczek133@gmail.com>
2021-08-07 17:10:23 +0200

    add this keyword to member function invocations, add do_print parameter to control verbose printing

--- test/benchmarks/vector_benchmark_base.hpp
+++ test/benchmarks/vector_benchmark_base.hpp
@@ -1,13 +1,15 @@
 #include <utils/utils.hpp>
 #include <benchmark/benchmark.h>
+#include <test_type/test_type.hpp>
+#include <boost/format.hpp>
 
 using namespace benchmark;
 
 template<template<class> class V, class... Ts>
 class vector_bench_env {
 public:
-    vector_bench_env(int seed) :
-        gen(seed) {}
+    vector_bench_env(int seed, bool do_print=false)
+        : gen(seed), do_print(do_print) {}
 
     void run_simulation(int iters=1000) {
         for (int i = 0; i < iters; ++i) {
@@ -16,13 +18,25 @@ public:
     }
 
 private:
+    template<class T>
+    void print_sizes() {
+        auto& typed_env = this->get_env_of_type<T>();
+        std::cout << typed_env.size() << ": ";
+        for (const auto& v : typed_env)
+            std::cout << v.size() << " ";
+        std::cout << std::endl;
+        for (const auto& v : typed_env)
+            std::cout << v.capacity() << " ";
+        std::cout << std::endl;
+    }
+
     size_t get_length_sum() {
         return (get_typed_length_sum<Ts>() + ...);
     }
 
     template<class T>
     size_t get_typed_length_sum() {
-        auto& typed_env = get_env_of_type<T>();
+        auto& typed_env = this->get_env_of_type<T>();
         size_t length_sum = 0;
 
         for (const auto& v : typed_env)
@@ -38,32 +52,34 @@ private:
             case 1: construct_action<T>(i); break;
             case 2: destroy_action<T>(i); break;
             case 3: push_back_action<T>(i); break;
-            case 4: pop_back_action<T>(i); break;
+            case 4: insert_action<T>(i); break;
             case 5: copy_action<T>(i); break;
-            case 6: insert_action<T>(i); break;
-            case 7: erase_action<T>(i); break;
+            case 6: erase_action<T>(i); break;
+            case 7: pop_back_action<T>(i); break;
         }
     }
 
     template<class T>
     void construct_action(int i) {
-        // std::cout << boost::format("construct_action(%d)\n") % i;
+        if (do_print)
+            std::cout << boost::format("construct_action(%d)\n") % i;
 
-        auto& typed_env = get_env_of_type<T>();
+        auto& typed_env = this->get_env_of_type<T>();
         int q = random(1, 3);
         std::uniform_int_distribution<> size_dist(1, i + 10);
 
         while (q--) {
-            typed_env.emplace_back(size_dist(gen));
+            typed_env.emplace_back(size_dist(this->gen));
             DoNotOptimize(typed_env.back().data());
         }
     }
 
     template<class T>
     void destroy_action(int i) {
-        // std::cout << boost::format("destroy_action(%d)\n") % i;
+        if (do_print)
+            std::cout << boost::format("destroy_action(%d)\n") % i;
 
-        auto& typed_env = get_env_of_type<T>();
+        auto& typed_env = this->get_env_of_type<T>();
         int typed_env_size = typed_env.size();
         if (typed_env_size == 0) {
             construct_action<T>(i);
@@ -82,9 +98,10 @@ private:
 
     template<class T>
     void push_back_action(int i) {
-        // std::cout << boost::format("push_back_action(%d)\n") % i;
+        if (do_print)
+            std::cout << boost::format("push_back_action(%d)\n") % i;
 
-        auto& typed_env = get_env_of_type<T>();
+        auto& typed_env = this->get_env_of_type<T>();
         int typed_env_size = typed_env.size();
         if (typed_env_size == 0) {
             construct_action<T>(i);
@@ -97,7 +114,7 @@ private:
         size_type<T> max_size = i * 100 + 1;
 
         while (q--) {
-            auto& picked = typed_env[pick_dist(gen)];
+            auto& picked = typed_env[pick_dist(this->gen)];
             auto can_put_in = picked.max_size() - picked.size();
             auto put_in = random<size_type<T>>(0, std::min(max_size, can_put_in));
 
@@ -108,9 +125,10 @@ private:
 
     template<class T>
     void pop_back_action(int i) {
-        // std::cout << boost::format("pop_back_action(%d)\n") % i;
+        if (do_print)
+            std::cout << boost::format("pop_back_action(%d)\n") % i;
 
-        auto& typed_env = get_env_of_type<T>();
+        auto& typed_env = this->get_env_of_type<T>();
         int typed_env_size = typed_env.size();
         if (typed_env_size == 0) {
             construct_action<T>(i);
@@ -121,7 +139,7 @@ private:
         std::uniform_int_distribution<> pick_dist(0, typed_env_size - 1);
 
         while (q--) {
-            auto& picked = typed_env[pick_dist(gen)];
+            auto& picked = typed_env[pick_dist(this->gen)];
             auto cnt = random<size_type<T>>(0, picked.size());
 
             while (cnt--)
@@ -131,9 +149,10 @@ private:
 
     template<class T>
     void copy_action(int i) {
-        // std::cout << boost::format("copy_action(%d)\n") % i;
+        if (do_print)
+            std::cout << boost::format("copy_action(%d)\n") % i;
 
-        auto& typed_env = get_env_of_type<T>();
+        auto& typed_env = this->get_env_of_type<T>();
         int typed_env_size = typed_env.size();
         if (typed_env_size == 0) {
             construct_action<T>(i);
@@ -144,7 +163,7 @@ private:
         std::uniform_int_distribution<> pick_dist(0, typed_env_size - 1);
 
         while (q--) {
-            auto pick = pick_dist(gen);
+            auto pick = pick_dist(this->gen);
 
             typed_env.emplace_back();
             typed_env.back() = typed_env[pick];
@@ -153,9 +172,10 @@ private:
 
     template<class T>
     void insert_action(int i) {
-        // std::cout << boost::format("insert_action(%d)\n") % i;
+        if (do_print)
+            std::cout << boost::format("insert_action(%d)\n") % i;
 
-        auto& typed_env = get_env_of_type<T>();
+        auto& typed_env = this->get_env_of_type<T>();
         int typed_env_size = typed_env.size();
         if (typed_env_size == 0) {
             construct_action<T>(i);
@@ -167,7 +187,7 @@ private:
         size_type<T> max_insert = i + 1;
 
         while (q--) {
-            auto& picked = typed_env[pick_dist(gen)];
+            auto& picked = typed_env[pick_dist(this->gen)];
             auto can_insert = picked.max_size() - picked.size();
             auto cnt = random<size_type<T>>(0, std::min(can_insert, max_insert));
             auto pos = random<size_type<T>>(0, picked.size());
@@ -178,9 +198,10 @@ private:
 
     template<class T>
     void erase_action(int i) {
-        // std::cout << boost::format("erase_action(%d)\n") % i;
+        if (do_print)
+            std::cout << boost::format("erase_action(%d)\n") % i;
 
-        auto& typed_env = get_env_of_type<T>();
+        auto& typed_env = this->get_env_of_type<T>();
         int typed_env_size = typed_env.size();
         if (typed_env_size == 0) {
             construct_action<T>(i);
@@ -191,7 +212,7 @@ private:
         std::uniform_int_distribution<> pick_dist(0, typed_env_size - 1);
 
         while (q--) {
-            auto& picked = typed_env[pick_dist(gen)];
+            auto& picked = typed_env[pick_dist(this->gen)];
             auto picked_size = picked.size();
             if (picked_size == 0)
                 continue;
@@ -205,7 +226,7 @@ private:
 
     template<class T>
     T random(T a, T b) {
-        return std::uniform_int_distribution<T>{a, b}(gen);
+        return std::uniform_int_distribution<T>{a, b}(this->gen);
     }
 
     template<class T>
@@ -226,6 +247,7 @@ private:
 private:
     std::mt19937 gen;
     std::tuple<env_container<V<Ts>>...> env;
+    bool do_print;
 };
 
 template<class Vector>
@@ -258,6 +280,8 @@ void BM_environment(State& s) {
     int iters = s.range(0);
     int r = 0;
 
+    test_type::start_recording();
+
     for (auto _ : s) {
         vector_bench_env<V, Ts...> v_env(seed + r++);
         DoNotOptimize(v_env);
@@ -266,6 +290,8 @@ void BM_environment(State& s) {
         
         ClobberMemory();
     }
+
+    test_type::print_stats();
 
     int id = s.range(1);
     s.counters["t" + std::to_string(id)];
