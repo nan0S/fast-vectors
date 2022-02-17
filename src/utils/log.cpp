34a0d6993567dfcc34d08e399a32d7d8745c3fcc
Hubert Obrzut <skoczek133@gmail.com>
2022-02-17 17:36:44 +0100

    init scripts branch

--- src/utils/log.cpp
+++
@@ -1,6 +0,0 @@
-#include "log.hpp"
-
-template<class First, class... Args>
-void log() {
-   std::cout << std::endl;
-}
--- src/utils/wrapper.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-template<class T>
-struct wrapper {
-    T obj;
-};
